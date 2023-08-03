c65ce78e083e0982b8ae15966705e89da2cb3eb5
Gregspi <gregspinetti@gmail.com>
2023-08-02 23:37:45 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React from "react";
+import './App.css';
+import Mapa from "./Mapa.svg"
+import {ReactComponent as MapaSVG} from "./Mapa.svg"
+
+function App() {
+
+  const [normalMap, setNormalMap] = React.useState(false);
+  const [renderedMap, setRenderedMap] = React.useState(false);
+
+  if(!normalMap && !renderedMap) return(
+    <div style={{ display: "flex", marginLeft:"auto", marginRight:"auto", maxWidth: "640px", justifyContent: "space-between", marginTop:"80px" }}
+    onClick={()=> {
+      // setNormalMap( false );
+      // setRenderedMap( false );
+    }}
+    >
+    <button variant="contained" loading={false} 
+    onClick={(e) => { 
+      e.preventDefault();
+      setNormalMap( true );
+    }
+    } sx={{ width: "231px", marginTop: "42px" }}>
+  Load normally
+  </button>
+
+  <button variant="contained" loading={false} 
+    onClick={(e) => {
+      e.preventDefault();
+      setRenderedMap( true );
+    }
+    }
+  sx={{ width: "231px", marginTop: "42px" }}>
+  Load fetching image
+  </button>
+  </div>
+  )
+
+  else if(renderedMap) return (
+    <img src={Mapa} alt="Mapa" />
+  )
+
+  else if (normalMap) return (
+  <MapaSVG />
+  )
+
+  return <div>Nothing here...</div>
+}
+
+export default App;
